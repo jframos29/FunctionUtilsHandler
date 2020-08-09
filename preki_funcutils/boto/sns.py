f198d4cdfc7e4b5eb438b5a851492e2421fdb96c
David Narvaez <david@gopreki.com>
2020-08-09 16:53:00 -0500

    Added SQS batch and return value

--- preki_funcutils/boto/sns.py
+++ preki_funcutils/boto/sns.py
@@ -4,7 +4,7 @@ from ..utils import stringify_message
 
 def send_topic_message(topic_arn, message, **kwargs):
     sns = boto3.resource('sns')
-    sns.publish(
+    return sns.publish(
         TargetArn=topic_arn,
         Message=stringify_message(message),
         **kwargs,
--- preki_funcutils/boto/sqs.py
+++ preki_funcutils/boto/sqs.py
@@ -5,7 +5,19 @@ from ..utils import stringify_message
 def enqueue_message(queue_name, message, **kwargs):
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=queue_name)
-    queue.send_message(
+    return queue.send_message(
         MessageBody=stringify_message(message),
+        **kwargs,
+    )
+
+
+def enqueue_messages_batch(queue_name, messages, **kwargs):
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+    return queue.send_messages(
+        Entries=[{
+            'Id': f'{i}',
+            'MessageBody': stringify_message(m)
+        } for i, m in enumerate(messages)],
         **kwargs,
     )
