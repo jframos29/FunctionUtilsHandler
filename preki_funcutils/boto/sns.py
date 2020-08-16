6d4f6974b52a98a0521e2c7c21a6fa563df3e2eb
David Narvaez <david@gopreki.com>
2020-08-16 18:43:35 -0500

    Fixed SNS topic client

--- preki_funcutils/boto/sns.py
+++ preki_funcutils/boto/sns.py
@@ -3,7 +3,7 @@ from ..utils import stringify_message
 
 
 def send_topic_message(topic_arn, message, **kwargs):
-    sns = boto3.resource('sns')
+    sns = boto3.client('sns')
     return sns.publish(
         TargetArn=topic_arn,
         Message=stringify_message(message),
